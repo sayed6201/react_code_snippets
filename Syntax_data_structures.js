27cb9b81d57133a8c351762e355cb9e6f7c05ada
sayed6201 <sayed.ahmed620@gmail.com>
2023-01-16 10:52:47 +0000

    ds added

--- Syntax_data_structures.js
+++ Syntax_data_structures.js
@@ -1,3 +1,7 @@
+//splcingi array - getting limited elements form an array
+const trommedTodos = todos.splice(0,10)
+
+
 //filtering and removingCommon Elements between two aeeay
 const A = [1, 2, 3, 4];
 const B = [2, 4];
