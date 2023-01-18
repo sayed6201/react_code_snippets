bf687469b645a2736463b758d3927460b4b7b228
sayed6201 <sayed.ahmed620@gmail.com>
2023-01-18 18:27:39 +0000

    snippets added

--- 1.Next-js/getServerSideProps.jsx
+++ 1.Next-js/getServerSideProps.jsx
@@ -1,4 +1,34 @@
 //=========================================================
+//Cookcies - Access coockies in getServerSideProps 
+//Tehn  fetch data  from server
+// ========================================================
+export async function getServerSideProps(context) {
+  let cookies = await context.req.headers.cookie
+  // req.cookies.get("token")
+  console.log('Cookeis found next', cookies)
+  console.log('ola', cookies)
+
+
+  if (typeof cookies !== 'string') {
+    return {
+      redirect: {
+      permanent: false,
+      destination: "auth/login",
+      },
+      props:{}, 
+    }
+  }   else 
+  return {
+    props: {},
+  }
+  // else {
+  //     return {
+  //         props: { auth: true },
+  //     }
+  // }
+}
+
+//=========================================================
 //Query-apram - Access query param from URL in getServerSideProps 
 //Teh  fetch data  from server
 // ========================================================
