2efbd733d7f5ffb251843d93e09fe488bf23eaf2
sayed6201 <sayed.ahmed620@gmail.com>
2023-02-10 15:21:15 +0000

    null checker

--- Syntax_js.js
+++ Syntax_js.js
@@ -1,5 +1,36 @@
+// ------------------------------------------------------------------------------------
+//null checker
+// ------------------------------------------------------------------------------------
 
+let y
+
+y = false || 'default'       // y = 'default'
+y = false ?? 'default'       // y = false
+
+y = 0n || 'default'          // y = 'default'
+y = 0n ?? 'default'          // y = 0n
+
+y = NaN || 'default'         // y = 'default'
+y = NaN ?? 'default'         // y = NaN
+
+y = '' || 'default'          // y = 'default'
+y = '' ?? 'default'          // y = ''
+// Since the new Nullish Coalescing Operator can differentiate between no value and a falsey value, it can be beneficial if you, for example, need to check if there is no String or an empty String. Generally, you probably want to use ?? instead of || most of the time.
+
+// Last and also least here are the two instances where they behave the same:
+
+let y
+
+y = null || 'default'        // y = 'default'
+y = null ?? 'default'        // y = 'default'
+
+y = undefined || 'default'   // y = 'default'
+y = undefined ?? 'default'   // y = 'default'
+
+
+// ------------------------------------------------------------------------------------
 // variable init
+// ------------------------------------------------------------------------------------
 let sort_by_param  = req.query.sort_by || 'date_of_creation'
 
 
