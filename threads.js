a2f1c0570b46891d95693316ac73dd5cfcbc10ed
sayed6201 <sayed.ahmed620@gmail.com>
2023-04-25 18:21:40 +0100

    promise timer snippet added

--- threads.js
+++ threads.js
@@ -1,3 +1,9 @@
+// promise timer
+await new Promise(resolve => setTimeout(resolve, 2000))
+
+
+
+// running multiple async function parallelly
 const getAnalyTicsData = async() =>{
     await Promise.all([
       getAppOpeningTime(),
